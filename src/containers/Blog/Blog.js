9e0a4c315d25e0ad9b97890c9c8505410af5ed23
Martin Mederly <martinmederly@gmail.com>
2020-08-30 12:50:31 +0200

    [164] Transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,9 +14,14 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({
-                    posts: response.data
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    };
                 });
+                this.setState({posts: updatedPosts});
                 console.log(response);
             });
     }
@@ -24,7 +29,7 @@ class Blog extends Component {
     render () {
         const posts = this.state.posts.map(
             post => {
-                return <Post title={post.title} key={post.id}/>
+                return <Post title={post.title} author={post.author} key={post.id}/>
             }
         );
 
