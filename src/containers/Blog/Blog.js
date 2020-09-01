4e3c0ad44ac848527f29dcd5e9f1a92e0d60bb7c
Martin Mederly <martinmederly@gmail.com>
2020-09-01 22:49:11 +0200

    [210] Redirecting Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 //import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from "./Posts/Posts";
-import {Route, NavLink, Switch} from 'react-router-dom'
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
 import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
@@ -35,6 +35,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
         );
